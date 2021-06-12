ee9cc0c49e864ef601ea05890ae2a1e6b24ce54c
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-12 17:04:02 +0530

    resumes data task on creation

--- PokemonEncyclopedia/PokemonEncyclopediaTests/RemoteFeedAPI/URLSessionHTTPClientTest.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/RemoteFeedAPI/URLSessionHTTPClientTest.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient{
 	func get(from url: URL){
 		session.dataTask(with: url) { _, _, _ in
 			
-		}
+		}.resume()
 	}
 	
 	
@@ -37,20 +37,43 @@ class URLSessionHTTPClientTest: XCTestCase {
 		XCTAssertEqual(session.receivedURLs, [url])
 	}
 	
+	func test_getFromURL_resumeDataTaskWithURL(){
+		let url = URL.init(string: "http://anyURL.com")!
+		let session = URLSessionSpy()
+		let task = URLSessionDataTaskSpy()
+		
+		
+		session.stub(url: url, task: task)
+		
+		let sut = URLSessionHTTPClient(session: session)
+		
+		sut.get(from: url)
+		
+		XCTAssertEqual(task.resumeCallCount, 1)
+	}
+	
 	
 	//MARK: - HELPER
 	private class URLSessionSpy: URLSession {
 		var receivedURLs = [URL]()
+		private var stub = [URL: URLSessionDataTask]()
+		
+		func stub(url: URL, task: URLSessionDataTaskSpy){
+			stub[url] = task
+		}
 		
 		override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
 			receivedURLs.append(url)
-			return URLSessionDataTaskSpy()
+			return stub[url] ?? URLSessionDataTaskSpy()
 		}
 		
 	}
 	
 	private class URLSessionDataTaskSpy: URLSessionDataTask {
-		
+		var resumeCallCount = 0
+		override func resume() {
+			resumeCallCount += 1
+		}
 	}
 	
 	
