6f04163db54542f91faebbe2daafc82353c14567
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-05 09:11:57 +0530

    save command doesn't request cache insertion on deletion error

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -22,9 +22,14 @@ class LocalFeedLoader {
 
 class FeedStore {
 	var deleteCachedFeedCallCount:Int = 0
+	var insertCallCount = 0
 	
 	func deleteCachedFeed(){
 		deleteCachedFeedCallCount += 1
+	}
+	
+	func completeDeletion(with error: Error, at index: Int = 0) {
+		
 	}
 }
 
@@ -43,6 +48,17 @@ class CacheFeedUseCaseTests: XCTestCase {
 		sut.save(items)
 		
 		XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+	}
+	
+	func test_save_doesnotRequestCacheInsertionOnDeletionError(){
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+		let deletionError = anyNSError()
+		
+		sut.save(items)
+		store.completeDeletion(with: deletionError)
+		
+		XCTAssertEqual(store.insertCallCount, 0)
 	}
 	
 	
@@ -64,5 +80,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 		return URL.init(string: "http://any-url.com")!
 	}
 
-	
+	private func anyNSError() -> NSError{
+		return  NSError(domain: "any error", code: 0, userInfo: nil)
+	}
 }
