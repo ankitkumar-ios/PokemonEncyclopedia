daad80b8232a8076a1ae0dc197941f7385929589
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-06 08:33:40 +0530

    save command fails on cache delete error

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -17,8 +17,9 @@ class LocalFeedLoader {
 		self.currentDate = currentDate
 	}
 	
-	func save(_ items:[FeedItem]){
+	func save(_ items:[FeedItem], completion: @escaping (Error?)->Void ){
 		store.deleteCachedFeed { [unowned self] error in
+			completion(error)
 			if error == nil {
 				self.store.insert(items, timestamp: currentDate())
 			}
@@ -68,7 +69,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 	func test_save_requestsCacheDelete(){
 		let (sut, store) = makeSUT()
 		let items = [uniqueItem(), uniqueItem()]
-		sut.save(items)
+		sut.save(items) { _ in }
 		
 		XCTAssertEqual(store.receivedMessage, [.deleteCachedFeed])
 	}
@@ -78,7 +79,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 		let items = [uniqueItem(), uniqueItem()]
 		let deletionError = anyNSError()
 		
-		sut.save(items)
+		sut.save(items) { _ in }
 		store.completeDeletion(with: deletionError)
 		
 		XCTAssertEqual(store.receivedMessage, [.deleteCachedFeed])
@@ -89,11 +90,34 @@ class CacheFeedUseCaseTests: XCTestCase {
 		let (sut, store) = makeSUT(currentDate: { timestamp })
 		let items = [uniqueItem(), uniqueItem()]
 		
-		sut.save(items)
+		sut.save(items) { _ in }
 		store.completeDeletionSuccessfully()
 		
 		XCTAssertEqual(store.receivedMessage, [.deleteCachedFeed, .insert(items, timestamp)])
 	}
+	
+	
+	func test_save_failOnDeletionError(){
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+		let deletionError = anyNSError()
+		var receivedError: Error?
+		
+		let exp = expectation(description: "wait for save")
+		
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+		store.completeDeletion(with: deletionError)
+		
+		wait(for: [exp], timeout: 1.0)
+		
+		
+		XCTAssertEqual(receivedError as NSError?, deletionError)
+	}
+	
+	
 	
 	
 	//MARK: Helper
