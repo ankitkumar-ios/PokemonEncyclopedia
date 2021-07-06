fb548ea7bd8fd8ebd53273c9a7ac2c8cfca3740a
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-06 08:45:15 +0530

    save command succeed on successful cache insertion

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -64,6 +64,10 @@ class FeedStore {
 		insertionCompletions[index](error)
 	}
 	
+	func completeInsertionSuccessfully(at index:Int = 0) {
+		insertionCompletions[index](nil)
+	}
+	
 }
 
 
@@ -147,6 +151,30 @@ class CacheFeedUseCaseTests: XCTestCase {
 		
 		XCTAssertEqual(receivedError as NSError?, insertionError)
 	}
+	
+	
+	
+	func test_save_succeedOnSuccessfulCacheInsertion(){
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+		let insertionError = anyNSError()
+		var receivedError: Error?
+		
+		let exp = expectation(description: "wait for save")
+		
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+		store.completeDeletionSuccessfully()
+		store.completeInsertionSuccessfully()
+		
+		wait(for: [exp], timeout: 1.0)
+		
+		
+		XCTAssertNil(receivedError)
+	}
+	
 	
 	
 	
