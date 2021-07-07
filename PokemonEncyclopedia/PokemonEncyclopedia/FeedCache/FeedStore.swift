7bc00d9389f148b29cde059f6b5767bbd7fdca34
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-07 08:57:25 +0530

    Move FeedStore to its own file

---
+++ PokemonEncyclopedia/PokemonEncyclopedia/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  PokemonEncyclopedia
+//
+//  Created by Ankit on 07/07/21.
+//
+
+import Foundation
+
+public protocol FeedStore {
+	typealias DeletionCompletion = (Error?)->Void
+	typealias InsertionCompletion = (Error?)->Void
+
+	func deleteCachedFeed(completion: @escaping DeletionCompletion)
+	func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
