785ba1d44d9c79aa57c663003c97734854625192
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-10 09:45:42 +0530

    Extract FeedCacheSpy into a shared scope to remove duplication

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -27,47 +27,5 @@ class LoadFeedFromcacheUseCaseTest: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
-	
-	private class FeedStoreSpy: FeedStore {
-		typealias DeletionCompletion = (Error?)->Void
-		typealias InsertionCompletion = (Error?)->Void
-		
-		enum ReceivedMessages: Equatable {
-			case deleteCachedFeed
-			case insert([LocalFeedImage], Date)
-		}
-		
-		private(set) var receivedMessage = [ReceivedMessages]()
-		
-		private var deletionCompletions = [DeletionCompletion]()
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		func deleteCachedFeed(completion: @escaping DeletionCompletion){
-			deletionCompletions.append(completion)
-			receivedMessage.append(.deleteCachedFeed)
-		}
-		
-		func completeDeletion(with error: Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index:Int = 0){
-			deletionCompletions[index](nil)
-		}
-		
-		func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-			insertionCompletions.append(completion)
-			receivedMessage.append(.insert(feed, timestamp))
-		}
-		
-		func completeInsertion(with error: Error, at index:Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func completeInsertionSuccessfully(at index:Int = 0) {
-			insertionCompletions[index](nil)
-		}
-		
-	}
 
 }
