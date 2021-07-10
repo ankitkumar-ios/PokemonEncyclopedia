f4b113438953640039c7274e0229296b73e342ec
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-10 12:13:09 +0530

    extract local members into properties

--- PokemonEncyclopedia/PokemonEncyclopedia/FeedCache/LocalFeedLoader.swift
+++ PokemonEncyclopedia/PokemonEncyclopedia/FeedCache/LocalFeedLoader.swift
@@ -10,6 +10,7 @@ import Foundation
 public class LocalFeedLoader {
 	private let store:FeedStore
 	private let currentDate: ()-> Date
+	private let calendar = Calendar(identifier: .gregorian)
 	
 	public typealias SaveResult = Error?
 	public typealias LoadResult = LoadFeedResult
@@ -53,9 +54,12 @@ public class LocalFeedLoader {
 		}
 	}
 	
+	private var maxcacheAgeInDays: Int {
+		return 7
+	}
+	
 	private func validate(_ timestamp: Date) -> Bool{
-		let calendar = Calendar(identifier: .gregorian)
-		guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+		guard let maxCacheAge = calendar.date(byAdding: .day, value: maxcacheAgeInDays, to: timestamp) else {
 			return false
 		}
 		
