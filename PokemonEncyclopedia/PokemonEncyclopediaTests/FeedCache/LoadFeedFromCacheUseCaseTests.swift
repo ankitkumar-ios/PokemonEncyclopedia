3721b4aa90dcbee0aa36ae46f09ebbd6a656406b
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-11 08:10:40 +0530

    deletes cache on retrieval error

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -77,6 +77,16 @@ class LoadFeedFromcacheUseCaseTest: XCTestCase {
 	}
 	
 	
+	func test_load_deletesCacheOnRetrievalError(){
+		let (sut, store) = makeSUT()
+		sut.load { _ in }
+		
+		store.completeRetrieval(with: anyNSError())
+		
+		XCTAssertEqual(store.receivedMessage, [.retrieval, .deleteCachedFeed])
+	}
+	
+	
 	//MARK: Helper
 	
 	private func makeSUT(currentDate: @escaping ()->Date = Date.init, file:StaticString = #file, line:UInt = #line)->(sut:LocalFeedLoader, store: FeedStoreSpy) {
