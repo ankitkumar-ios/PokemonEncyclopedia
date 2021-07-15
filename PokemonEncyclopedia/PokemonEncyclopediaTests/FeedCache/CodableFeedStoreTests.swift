24e53d48da5f14b9cf45ca12e021c5f759074723
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-15 09:39:46 +0530

    retrieve delivers failure on retrieval error

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CodableFeedStoreTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CodableFeedStoreTests.swift
@@ -45,10 +45,14 @@ class CodableFeedStore {
 		guard let data = try? Data(contentsOf: storeURL) else{
 			return completion(.empty)
 		}
-		
-		let decoder = JSONDecoder()
-		let cache = try! decoder.decode(Cache.self, from: data)
-		completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+		do {
+			let decoder = JSONDecoder()
+			let cache = try decoder.decode(Cache.self, from: data)
+			completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+		}
+		catch{
+			completion(.failure(error))
+		}
 	}
 	
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -107,6 +111,13 @@ class CodableFeedStoreTests: XCTestCase {
 		expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
 	}
 	
+	func test_retrieve_deliversFailureOnRetrievalError(){
+		let sut = makeSUT()
+
+		try! "invalid Data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+		
+		expect(sut, toRetrieve: .failure(anyNSError()))
+	}
 	
 	
 	//MARK:- Helper
@@ -132,7 +143,8 @@ class CodableFeedStoreTests: XCTestCase {
 		
 		sut.retrieve { receivedResult in
 			switch (expectedResult, receivedResult) {
-				case (.empty, .empty):
+				case (.empty, .empty),
+						 (.failure, .failure):
 				break
 					
 				case let (.found(expectedFeed, expectedTimestamp), .found(receivedFeed, receivedTimestamp)):
