8f3e1f27a1f9231084f955f5ea21450dfd279973
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-17 08:18:38 +0530

    Deleting non-empty cache empties previously inserted cache

--- PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CodableFeedStoreTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/CodableFeedStoreTests.swift
@@ -68,8 +68,17 @@ class CodableFeedStore {
 	}
 	
 	func deleteCacheFeed(completion: @escaping FeedStore.DeletionCompletion){
-		completion(nil)
+		guard FileManager.default.fileExists(atPath: storeURL.path) else {
+			return completion(nil)
+		}
 		
+		do {
+			try FileManager.default.removeItem(at: storeURL)
+			completion(nil)
+		}
+		catch{
+			completion(error)
+		}
 	}
 }
 
@@ -166,6 +175,17 @@ class CodableFeedStoreTests: XCTestCase {
 	
 	func test_delete_hasNoSideEffectsOnEmptyCache(){
 		let sut = makeSUT()
+
+		let deleteError = deleteCache(from: sut)
+		XCTAssertNil(deleteError, "Expected empty cache deletion to succeed")
+		
+		expect(sut, toRetrieve: .empty)
+	}
+	
+
+	func test_delete_emptiesPreviouslyInsertedCache(){
+		let sut = makeSUT()
+		insert((uniqueImageFeed().local, Date()), to: sut)
 
 		let deleteError = deleteCache(from: sut)
 		XCTAssertNil(deleteError, "Expected empty cache deletion to succeed")
