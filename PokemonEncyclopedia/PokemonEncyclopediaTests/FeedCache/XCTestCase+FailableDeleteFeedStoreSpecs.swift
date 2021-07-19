a124f1da8ee41101725f7d745b7644225bd2c9f0
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-19 09:28:11 +0530

    Group feed store specs and helpers

---
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,26 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  PokemonEncyclopediaTests
+//
+//  Created by Ankit on 19/07/21.
+//
+
+import XCTest
+import PokemonEncyclopedia
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+	
+	func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		let deletionError = deleteCache(from: sut)
+
+		XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+	}
+	
+	
+	func assertThatDeletehasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		deleteCache(from: sut)
+
+		expect(sut, toRetrieve: .empty, file: file, line: line)
+	}
+	
+}
---
+++ PokemonEncyclopedia/PokemonEncyclopediaTests/FeedCache/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -0,0 +1,34 @@
+//
+//  XCTestCase+FailableInsertFeedStoreSpecs.swift
+//  PokemonEncyclopediaTests
+//
+//  Created by Ankit on 19/07/21.
+//
+
+import XCTest
+import PokemonEncyclopedia
+
+
+extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
+	
+	func assertThatInsertDeliversErrorOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		let feed = uniqueImageFeed().local
+		let timestamp = Date()
+		
+		let insertionError = insert((feed, timestamp), to: sut)
+		
+		XCTAssertNotNil(insertionError, "Expect cache insertion error", file: file, line: line)
+	}
+	
+	
+	func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+		let feed = uniqueImageFeed().local
+		let timestamp = Date()
+		
+		insert((feed, timestamp), to: sut)
+		
+		expect(sut, toRetrieve: .empty, file: file, line: line)
+	}
+	
+	
+}
