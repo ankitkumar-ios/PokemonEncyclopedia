831d9bddb09a07cffb754b05464bd2e594cbf0ae
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-25 22:39:14 +0530

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ PokemonEncyclopedia/PokemonEncyclopediaIntegrationTests/PokemonEncyclopediaIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonEncyclopediaIntegrationTests.swift
+//  PokemonEncyclopediaIntegrationTests
+//
+//  Created by Ankit on 25/07/21.
+//
+
+import XCTest
+
+class PokemonEncyclopediaIntegrationTests: XCTestCase {
+
+
+
+}
