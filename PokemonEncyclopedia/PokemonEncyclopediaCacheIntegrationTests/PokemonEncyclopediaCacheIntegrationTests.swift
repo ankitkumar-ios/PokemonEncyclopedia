cb2d5bc2327dc21830de3adf0c27c06a1853d376
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-26 07:52:21 +0530

    LocalFeedLoader in integration with the CoreDataFeedStore delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- PokemonEncyclopedia/PokemonEncyclopediaCacheIntegrationTests/PokemonEncyclopediaCacheIntegrationTests.swift
+++ PokemonEncyclopedia/PokemonEncyclopediaCacheIntegrationTests/PokemonEncyclopediaCacheIntegrationTests.swift
@@ -36,8 +36,38 @@ class PokemonEncyclopediaCacheIntegrationTests: XCTestCase {
 	}
 	
 	
+	func test_load_deliversItemsSaveOnASeparateInstace(){
+		let sutToPerformSave = makeSUT()
+		let sutToPerformLoad = makeSUT()
+		let feed = uniqueImageFeed().models
+		
+		let exp = expectation(description: "Wait for saving cache")
+		
+		sutToPerformSave.save(feed) { saveError in
+			XCTAssertNil(saveError,"Expected successful result")
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+		
+		let loadExp = expectation(description: "Wait for load completion")
+		sutToPerformLoad.load { result in
+			switch result {
+				case let .success(imageFeed):
+					XCTAssertEqual(imageFeed, feed)
+				case let .failure(error):
+					XCTFail("Expected successful result, got \(error) instead")
+			}
+			loadExp.fulfill()
+		}
+		
+		wait(for: [loadExp], timeout: 1.0)
+		
+	}
+	
+	
+	
 	//MARK:- Helper
-	func makeSUT() -> LocalFeedLoader {
+	private func makeSUT() -> LocalFeedLoader {
 		let storeBundle = Bundle(for: CoreDataFeedStore.self)
 		let storeURL = testSpecificURL()
 		let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
@@ -66,7 +96,7 @@ class PokemonEncyclopediaCacheIntegrationTests: XCTestCase {
 		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
 	}
 
-	func cacheDirectory() -> URL {
+	private func cacheDirectory() -> URL {
 		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
 	}
 
