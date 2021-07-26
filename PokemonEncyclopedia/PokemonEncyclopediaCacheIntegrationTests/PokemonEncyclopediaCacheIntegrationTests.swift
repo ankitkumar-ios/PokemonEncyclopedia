df65c10a777e063a0f8c18625785c465b5322a99
Ankit Kumar <ankit.kumar@truminds.com>
2021-07-26 07:22:38 +0530

    MacOS unit test removed, iOS Unit tests added

---
+++ PokemonEncyclopedia/PokemonEncyclopediaCacheIntegrationTests/PokemonEncyclopediaCacheIntegrationTests.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonEncyclopediaCacheIntegrationTests.swift
+//  PokemonEncyclopediaCacheIntegrationTests
+//
+//  Created by Ankit on 26/07/21.
+//
+
+import XCTest
+
+class PokemonEncyclopediaCacheIntegrationTests: XCTestCase {
+
+	func test_load_deliversNoItemsOnEmptyCache() {
+		let sut = makeSUT()
+		let exp = expectation(description: "Wait for completion")
+		
+		
+	}
+	
+	
+	//MARK:- Helper
+	func makeSUT() -> LoadFeedLoader {
+		
+	}
+
+
+}
